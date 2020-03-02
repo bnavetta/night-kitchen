9694dab0a2b615e15d84c8279322b422da22ed1e
Ben Navetta <ben.navetta@gmail.com>
2020-03-01 22:37:22 -0800

    Fix version setting

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,15 +4,21 @@ set -eou pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo "Working directory is not clean!"
+    echo "Working directory is not clean!" > &2
     exit 1
 fi
 
-version=$(cargo read-manifest | jq -r '.version')
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <version>" > &2
+    exit 1
+fi
+
+version="$1"
 
 echo "Releasing v$version"
 
-sed -i "s/^pkgver=.*$/pkgver='$version'/" packaging/arch/PKGBUILD
+sed -i "s/^pkgver=.*/pkgver='$version'/" packaging/arch/PKGBUILD
+cargo bump "$version"
 
 git add .
 git commit -m "Release v$version"
